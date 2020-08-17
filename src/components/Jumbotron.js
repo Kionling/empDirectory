964cdfd797b714938bf39d73491b50d2877a745d
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-17 09:50:18 -0700

    adding navbar:

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,8 +1,16 @@
-import React from "react"
+import React from "react";
 
-
-function Jumbotron(){
-
+function Jumbotron() {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Employee Directory</h1>
+        <p class="lead">
+          This is an application that allows the user to search for specfic employees!
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Jumbotron
+export default Jumbotron;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,11 @@
-import React from "react"
+import React from "react";
 
-function Navbar(){
-
+function Navbar() {
+  return (
+    <nav class="navbar navbar-dark bg-dark">
+      <span class="navbar-brand mb-0 h1">Employee Directory</span>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
