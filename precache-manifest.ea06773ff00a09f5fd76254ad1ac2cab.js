a4c1e4c6474a6d1c0ed9ec353f2f8db6ed7abcaa
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-17 11:59:17 -0700

    Updates

---
+++ precache-manifest.ea06773ff00a09f5fd76254ad1ac2cab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f531ccc9801608610a1bec793e90f915",
+    "url": "/empDirectory/index.html"
+  },
+  {
+    "revision": "d2bc46b2c35e7504d89d",
+    "url": "/empDirectory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eebd8a3d7d3a141874e0",
+    "url": "/empDirectory/static/js/2.71d2dec8.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/empDirectory/static/js/2.71d2dec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bc46b2c35e7504d89d",
+    "url": "/empDirectory/static/js/main.02dbbf2f.chunk.js"
+  },
+  {
+    "revision": "96469e0536abb14f2a83",
+    "url": "/empDirectory/static/js/runtime-main.97066c96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/empDirectory/static/media/logo.5d5d9eef.svg"
+  }
+]);
