36fd4938d09dc59efbf58cafd832766b12096e49
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-17 10:32:26 -0700

    Created function that searches for employees as input is typed

--- src/pages/GlobalState.js
+++ src/pages/GlobalState.js
@@ -1,47 +1,54 @@
 import React, { useState } from "react";
 
-import SearchBar from "../components/search"
+import SearchBar from "../components/search";
 
-import employees from "../employees.json"
+import employees from "../employees.json";
 
+function Workplace() {
+  const [sort, setSort] = useState({
+    name: true,
+    department: true,
+    phone: true,
+    employees: employees,
+  });
 
-function Workplace(){
-    const [sort, setSort] = useState({  
-        name: true, 
-        department: true,
-        phone: true
-
-    })
-
-    const [state, dispatch] = useReducer(
-        (state, action) => {
-         switch (action.type) {
-            case "renderEmployees":
-                return [...employees]
-            case "search":
-                return employees.filter((employee) =>{
-                    return (
-                        employee.name.includes(action.input),
-                        employee.department.includes(action.input),
-                        employee.phone.includes(action.input)
-
-                    )
-                })
-
-         }
-
-        },
-        
-      );
-
-    search = (event) => {
-        const userInput = event.target.value
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "renderEmployees":
+        return [...employees];
+      case "search":
+        return employees.filter((employee) => {
+          return (
+            employee.name.includes(action.input),
+            employee.department.includes(action.input),
+            employee.phone.includes(action.input)
+          );
+        });
     }
+  });
 
+  search = (event) => {
+    const userInput = event.target.value;
+    if (userInput === "") {
+      this.setState({
+        employees: [...employees],
+      });
+    } else {
+      this.setState({
+        employees: this.state.employees.filter((employee) => {
+          if (employees.name.includes(userInput)) {
+            return true;    
+          } else {
+            return false;
+          }
+        }),
+      });
+    }
+  };
 
-
-
-
+  intialSetup = (event) => {
+      
+  }
 }
 
-export default Workplace
+export default Workplace;
