7bbd91f849cdf61c6238b8922a4621aa1300b8d4
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-17 10:08:22 -0700

    Creating switch cases for reducer

--- src/pages/GlobalState.js
+++ src/pages/GlobalState.js
@@ -13,18 +13,12 @@ function Workplace(){
 
     })
 
-    // const [state, dispatch] = useReducer(//we define what are valid actions within out dispatch. What changes we CAN make only within Dispatch and only with valid action
-    //     (state, action) => {
-    //       if (action === "praiseHarry") {
-    //         return { ...state, HarryPraises: state.HarryPraises + 1 };
-    //       } else if (action === "praiseHermione") {
-    //         return { ...state, HermionePraises: state.HermionePraises + 1 };
-    //       } else {
-    //         return state;
-    //       }
-    //     },
-    //     { HarryPraises: 0, HermionePraises: 0 }
-    //   );
+    const [state, dispatch] = useReducer(
+        (state, action) => {
+         switch (action.type)
+        },
+        []
+      );
 
 
 
