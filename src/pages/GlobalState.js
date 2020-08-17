bee7d166ca7346ec035484dfb50b72f068270a1e
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-17 10:04:17 -0700

    importing useReducer example

--- src/pages/GlobalState.js
+++ src/pages/GlobalState.js
@@ -12,12 +12,21 @@ function Workplace(){
         phone: true
 
     })
-return(
-    
-    
+
+    // const [state, dispatch] = useReducer(//we define what are valid actions within out dispatch. What changes we CAN make only within Dispatch and only with valid action
+    //     (state, action) => {
+    //       if (action === "praiseHarry") {
+    //         return { ...state, HarryPraises: state.HarryPraises + 1 };
+    //       } else if (action === "praiseHermione") {
+    //         return { ...state, HermionePraises: state.HermionePraises + 1 };
+    //       } else {
+    //         return state;
+    //       }
+    //     },
+    //     { HarryPraises: 0, HermionePraises: 0 }
+    //   );
 
 
-)
 
 
 
